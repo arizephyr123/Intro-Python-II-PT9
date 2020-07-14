b6afce4014ba3f62d6518b05bfc1ef6b72307048
Arielle Hollingsworth <arizephyr123@gmail.com>
2020-07-13 20:57:18 -0700

    Player class created

--- src/player.py
+++ src/player.py
@@ -1,2 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, curr_loc, name):
+        self.name = name
+        self.curr_loc = curr_loc
+
+    def __str__(self):
+        return f'Player: {self.name} -> location: {self.curr_loc}'
