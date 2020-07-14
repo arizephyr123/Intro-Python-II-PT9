94c4688da46b235908c430d6e995a14a68f403e7
Arielle Hollingsworth <arizephyr123@gmail.com>
2020-07-13 20:40:18 -0700

    Room class created, pipenv setup

---
+++ examples/lecture/store.py
@@ -0,0 +1,17 @@
+class Store:
+    def __init__(self, name, categories):
+        self.name = name
+        self.categories = []
+
+    def __str__(self):
+        for cat in self.categories:
+            pass
+        return f'TODO'
+
+    def __repr__(self):
+        # for other developers like for debugging
+        return f'TODO'
+
+
+
+from category import Category
